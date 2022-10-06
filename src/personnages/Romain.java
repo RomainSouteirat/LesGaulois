924ef76cd8da000c8271cd37236521a6da5f5683
RomainSouteirat <romainsout@gmail.com>
2022-10-06 15:31:09 +0200

    fin cour

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,40 @@
 package personnages;
 
 public class Romain {
-
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+	
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} 	
+		else {
+			parler("J'abandonne...");
+		}
+	}
+	public static void main(String[] args) {
+		Romain minus = new Gaulois("Minus", 8);
+		System.out.println(asterix);
+		System.out.println(asterix.prendreParole());
+		System.out.println(asterix.parler("je parle"));
+	
+	}
 }
