16bd34049f743a9c66058df03b80661e9e6da3b2
RomainSouteirat <romainsout@gmail.com>
2023-09-19 17:42:26 +0200

    debut tp1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,10 +1,27 @@
 package personnages;
 
 public class Gaulois {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+	
+	}
+	
 }
+
